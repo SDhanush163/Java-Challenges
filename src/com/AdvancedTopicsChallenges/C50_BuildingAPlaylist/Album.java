e3d214b63a4fdc684f94ef79d69fa3549c877690
Dhanush <sdhanush163@gmail.com>
2025-08-30 16:55:37 +0530

    Playlist with Nesting classes

---
+++ src/com/AdvancedTopicsChallenges/C50_BuildingAPlaylist/Album.java
@@ -0,0 +1,69 @@
+package com.AdvancedTopicsChallenges.C50_BuildingAPlaylist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    public static class SongList {
+        private ArrayList<Song> songs;
+
+        private SongList() {
+            this.songs = new ArrayList<>();
+        }
+
+        private boolean add(Song song) {
+            if (songs.contains(song)) return false;
+            songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song song : songs) if (song.getTitle().equals(title)) return song;
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+            if ((index > 0) && (index < songs.size()))
+                return songs.get(index);
+            return null;
+        }
+    }
+
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        return songs.add(new Song(title, duration));
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+        Song song = songs.findSong(trackNumber);
+        if (song == null) {
+            System.out.println("This album does not have a track " + trackNumber);
+            return false;
+        }
+        playlist.add(song);
+        return true;
+
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song song = songs.findSong(title);
+        if (song == null) {
+            System.out.println("The song " + title + " is not in this album");
+            return false;
+        }
+        playlist.add(song);
+        return true;
+    }
+
+}
+
---
+++ src/com/AdvancedTopicsChallenges/C50_BuildingAPlaylist/Song.java
@@ -0,0 +1,21 @@
+package com.AdvancedTopicsChallenges.C50_BuildingAPlaylist;
+
+public class Song {
+
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", title, duration);
+    }
+}
